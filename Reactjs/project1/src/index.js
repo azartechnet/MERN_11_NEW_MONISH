cf555203467595d9989b9fe61617690c07b9ce8d
azartechnet <azartechnet@gmail.com>
2025-01-18 11:50:20 +0530

    updated

---
+++ Reactjs/project1/src/index.js
@@ -0,0 +1,72 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+//basic rendering
+/*const App=()=>{
+    return( 
+        <div>
+            <h1>This is First Program</h1>
+        </div>
+    )
+}
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);*/
+
+//Function Rendering
+
+/*function Sample()
+{
+    return(
+        <>
+         <h1>Heading-1</h1>
+         <h2>Heading-2</h2>
+        </>
+     
+    )
+}
+const r1 = ReactDOM.createRoot(document.getElementById('root'));
+r1.render(<Sample/>);*/
+
+//Function Rendering using login form
+
+/*function Login()
+{
+    return(
+        <div>
+            <h1>Login Form</h1>
+            <label>UserName</label>
+            <input type="text"/>
+            <br/>
+            <label>Password</label>
+            <input type="password"/>
+            <br/>
+            <input type="submit"/>
+        </div>
+    )
+}
+const r2 = ReactDOM.createRoot(document.getElementById('root'));
+r2.render(<Login/>);*/
+
+//Dynamic Rendering
+
+/*const App=()=>{
+    const username="sdlc";
+    return(
+        <div>
+            <h1>Dynamic Rendering</h1>
+            <h2>Username: {username}</h2>
+            <p>Today's date::{new Date().toLocaleDateString()}</p>
+        </div>
+    )
+}
+const r3 = ReactDOM.createRoot(document.getElementById('root'));
+r3.render(<App/>);*/
+
+//React without JSX
+
+const myelem1=React.createElement('h1',null,'welcome')
+const myelem2=React.createElement('h2',null,'welcome to react')
+const myelem3=React.createElement('p',null,'welcome to react')
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(React.createElement("div", null, myelem1, myelem2, myelem3));
+
+//Task unorderlist and orderlist
