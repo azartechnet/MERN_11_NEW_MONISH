2f4144e360872524e7617103225d4afb47deca1a
azartechnet <azartechnet@gmail.com>
2025-06-23 16:08:56 +0530

    updated

--- MySqlCrud/react-mysql-crud-client/src/PostsList.js
+++ MySqlCrud/react-mysql-crud-client/src/PostsList.js
@@ -1,6 +1,47 @@
+import React,{useEffect,useState} from "react";
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 const PostsList=()=>{
+
+    const [posts,setPosts]=useState([]);
+
+    useEffect(()=>{
+        axios.get('http://localhost:3001/getposts')
+        .then(response=>setPosts(response.data))
+    },[])
+
     return(
-        <h1>This is Post List</h1>
+       <div className="container mt-5">
+            <h2>PostsList</h2>
+            <table className="table table-striped">
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>title</th>
+                        <th>Body</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        posts.map(
+                            post=>(
+                                <tr key={post.id}>
+                                    <td>{post.id}</td>
+                                    <td>{post.title}</td>
+                                    <td>{post.body}</td>
+                                    <td>
+                                        <button type="button">Edit</button>
+                                        <button type="button">Delete</button>
+                                    </td>
+
+                                </tr>
+                            )
+                        )
+                    }
+                </tbody>
+            </table>
+       </div>
     )
     }
 export default PostsList;
--- MySqlCrud/server.js
+++ MySqlCrud/server.js
@@ -46,6 +46,18 @@ app.post('/addpost', (req, res) => {
     });
 });
 
+//getPosts
+
+app.get("/getposts",(req,res)=>{
+    let sql="SELECT * FROM posts";
+    db.query(sql,(err,result)=>
+        {
+            if(err)
+                throw err;
+            res.send(result);
+            });
+})
+
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
